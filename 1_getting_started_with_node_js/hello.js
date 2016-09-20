156f5c79b2df57fa249436730eb4ee630f7c22ca
Ali Tha'Ban <alithaban@gmail.com>
2016-09-20 21:59:55 +1000

    Added the 3rd lesson.

---
+++ 1_getting_started_with_node_js/hello.js
@@ -0,0 +1,4 @@
+/**
+ * Created by root on 20/09/16.
+ */
+console.log('Hello World');
---
+++ 1_getting_started_with_node_js/lesson_3/time.js
@@ -0,0 +1,9 @@
+/**
+ * Created by root on 20/09/16.
+ */
+
+// Run npm install moment
+
+var moment = require('moment');
+
+console.log(moment().format('DD/MM/YYYY'));
