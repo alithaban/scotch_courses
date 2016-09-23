68d72872b1c3970b54256e3b6d12a4cd8b23b33e
Ali Tha'Ban <alithaban@gmail.com>
2016-09-24 07:24:41 +1000

    Added lesson 12

--- 1_getting_started_with_node_js/lesson_6/app/routes.js
+++ 1_getting_started_with_node_js/lesson_6/app/routes.js
@@ -1,10 +1,9 @@
+var core = require('./controllers/core');
+
 module.exports = function(app) {
-    app.get('/',function (req, res) {
-        res.send('This is the homepage');
-    })
-    app.get('/top',function (req, res) {
-        res.send('This is the top page');
-    })
+    app.get('/', core.home)
+
+    app.get('/top', core.top)
     app.get('/results',function (req, res) {
         res.send('This is the results page');
     })
