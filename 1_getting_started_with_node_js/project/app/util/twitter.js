1fdde3dcf5cd12c6088039c92e9ea2d5c55d1f85
Ali Tha'Ban <alithaban@gmail.com>
2016-09-25 10:59:01 +1000

    Added lesson 16.

--- 1_getting_started_with_node_js/project/app/util/twitter.js
+++ 1_getting_started_with_node_js/project/app/util/twitter.js
@@ -1,3 +1,13 @@
 /**
  * Created by root on 20/09/16.
  */
+var twitter = require('twitter');
+
+var client = new twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_srcret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+})
+
+module.exports = client;
