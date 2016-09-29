72cb5d3cd1f9c7b1ff30999a70deb4d51bfbe1d0
Ali Tha'Ban <alithaban@gmail.com>
2016-09-30 08:17:00 +1000

    Added lesson 2.

--- 2_routing_node_applications/server.js
+++ 2_routing_node_applications/server.js
@@ -9,8 +9,17 @@ var app = require('express')(),
 app.use(morgan('dev'));
 // Set routes
 app.get('/', function (req, res) {
-    res.send('I\'m the app');
+    res.sendFile(__dirname + '/index.html');
 })
+
+app.get('/about', function (req, res) {
+    res.json({message:'I\'m the about page'});
+})
+
+app.get('/contact', function (req, res) {
+    res.send('I\'m the contact');
+})
+
 //start the server
 app.listen(port, function () {
     console.log('Te app is running');
