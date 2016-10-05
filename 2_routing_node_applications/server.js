f9d60d8c2a2935733135c28cdd620ae1f9e5a19d
Ali Tha'Ban <alithaban@gmail.com>
2016-10-06 08:24:52 +1100

    Added lesson 5.

--- 2_routing_node_applications/server.js
+++ 2_routing_node_applications/server.js
@@ -9,10 +9,14 @@ var app = require('express')(),
 // Configuration
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:true}));
+
+
 // Set routes
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
+
+app.use(authenticate);
 
 app.get('/about', function (req, res) {
     res.json({message:'I\'m the about page'});
@@ -24,10 +28,24 @@ app.route('/contact').get(function (req, res) {
     res.send('Hello ' + req.body.name);
 })
 
-app.get('/:username/:post_slug',function (req,res) {
+app.get('/:username/:post_slug', function (req,res) {
     console.log(req.params);
     res.send(' Written by ' + req.params.username + ', ' + req.params.post_slug);
 })
+
+function authenticate(req,res, next){
+    console.log('Authenticate is here');
+
+    next();
+}
+
+function checkName(req,res,next){
+    console.log(req.params);
+
+    // Validate the user
+
+    next();
+}
 
 //start the server
 app.listen(port, function () {
