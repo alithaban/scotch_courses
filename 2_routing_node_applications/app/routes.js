b0c137fccc40cb6752c7cfc3a558f8c06efda0a8
Ali Tha'Ban <alithaban@gmail.com>
2016-10-06 18:04:10 +1100

    Added lesson 8.

--- 2_routing_node_applications/app/routes.js
+++ 2_routing_node_applications/app/routes.js
@@ -11,26 +11,36 @@ module.exports = router;
 
 // Apply the routes to the router
 // Set routes
-router.get('/', function (req, res) {
+router.get('/', showHome);
+router.get('/about', showAbout);
+router.get('/contact', showContactForm);
+router.post('/contact', processContactForm);
+router.get('/:username/:post_slug', showProfile);
+router.use(show404);
+
+function showHome(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'));
-})
+}
 
-router.get('/about', function (req, res) {
+function showAbout(req, res) {
     res.json({message:'I\'m the about page'});
-})
+}
 
-router.route('/contact').get(function (req, res) {
+function showContactForm(req, res) {
     res.sendFile(path.join(__dirname,'../contact.html'));
-}).post(function (req, res) {
-    res.send('Hello ' + req.body.name);
-})
+}
 
-router.get('/:username/:post_slug', function (req,res) {
+function processContactForm(req, res) {
+    res.send('Hello ' + req.body.name);
+}
+
+function showProfile(req, res) {
     console.log(req.params);
     res.send(' Written by ' + req.params.username + ', ' + req.params.post_slug);
-})
+}
 
-router.use(function (req, res, next) {
+
+function show404(req, res) {
     res.status(404);
     res.sendFile(path.join(__dirname,'../404.html'));
-})
+}
